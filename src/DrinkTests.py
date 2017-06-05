6f6a86b28f4f6cf705425f6e0d4d8a9ec6fa3595
Tom Evans <TomosAMEvans@gmail.com>
2017-06-05 20:56:05 +0100

    Added some tests for Drink

---
+++ src/DrinkTests.py
@@ -0,0 +1,23 @@
+import unittest
+from Drink import Drink
+
+class TestMeasuresMethods(unittest.TestCase):
+    def testDrinkEquality(self):
+        d1 = Drink("testDrink1", ["ing1", "ing2", "ing3"])
+        d2 = Drink("testDrink2", ["ing1", "ing2", "ing3"])
+        d3 = Drink("testDrink1", ["ing1"])
+        self.assertTrue(d1==d3)
+        self.assertFalse(d1==d2)
+
+    def testDrinkIterator(self):
+        d1 = Drink("testDrink1", ["ing1", "ing2", "ing3"])
+        lst = []
+        for ingredient in d1:
+            lst.append(ingredient)
+        self.assertEqual(lst, ["ing1", "ing2", "ing3"])
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
