d08e83820612892736b0f960ea712f50073ae3ff
Tom Evans <TomosAMEvans@gmail.com>
2017-11-20 18:01:04 +0000

    client

--- src/client.py
+++ src/client.py
@@ -6,8 +6,8 @@ vr = {
 }
 
 
-menu = requests.get('http://10.11.12.41:5000/drinks', json=vr).json()
-print(menu)
+# menu = requests.get('http://10.11.12.41:5000/drinks', json=vr).json()
+# print(menu)
 menu = requests.put('http://10.11.12.41:5000/shutdown', json=vr).json()
 
 
