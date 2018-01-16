b9f5e6a9cfbad627d40eb1f9dd5db7890026362e
Tom Evans <TomosAMEvans@gmail.com>
2018-01-16 16:00:24 +0000

    Added units to dispensers

--- src/Measures.py
+++ src/Measures.py
@@ -35,14 +35,15 @@ class Unit():
 
     @classmethod
     def fromJson(cls, json):
+        print('JSON: ' + str(json))
         print('in from json')
         name = json['unit']
         print('name: ' + name)
         print('hereerererererere')
-        print('json is: ' + json)
-        print('maybe: ' + json['amount'])
+        print('json is: ' + str(json))
+        print('maybe: ' + str(json['amount']))
         amount = int(json['amount'])
-        print('amount: ' + amount)
+        print('amount: ' + str(amount))
 
         if name == 'shots':
             return Shot(amount)
